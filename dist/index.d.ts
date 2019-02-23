21dccd6f871ec63c79ef5875afd34bd5b89fb057
durangatan <joeduran8@gmail.com>
2019-02-23 12:36:56 -0600

    expose SetType

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@ export { default as Queryable, QueryableId } from './Queryable';
 export { default as Exercise, ExerciseArguments, ExerciseId } from './Exercise';
 export { default as Routine, RoutineArguments, RoutineId } from './Routine';
 export { default as RoutineSet, RoutineSetArguments, RoutineSetId } from './RoutineSet';
-export { default as WorkoutSet, WorkoutSetArguments, WorkoutSetId } from './WorkoutSet';
+export { default as WorkoutSet, WorkoutSetArguments, WorkoutSetId, SetType } from './WorkoutSet';
 export { default as Workout, WorkoutArguments, WorkoutId } from './Workout';
 export { default as WorkoutRoutine, WorkoutRoutineArguments, WorkoutRoutineId } from './WorkoutRoutine';
 export { default as CompletedSet, CompletedSetArguments, CompletedSetId } from './CompletedSet';
